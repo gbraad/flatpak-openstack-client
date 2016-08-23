3bfaba6355dd3da78f86a7c44d970baab7e2c792
Gerard Braad <me@gbraad.nl>
2016-08-23 11:11:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flatpak build-init openstackclient org.openstack.Client org.gnome.Sdk org.gnome.Platform 3.20
 flatpak build-finish openstackclient --share=network --command=openstack
-flatpak build openstackclient pip install -U pip; pip install --user python-openstackclient
+flatpak build openstackclient pip install virtualenv; python -m virtualenv client; source client/bin/activate; pip install -U pip setuptools; pip install python-openstackclient
